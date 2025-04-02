1f813c9225cacab2001bf14f107793fb266962ba
Darsini-Chandrababu <darsinichandrababucm@gmail.com>
2025-04-02 14:56:25 +0530

    Initial Commit

---
+++ demo/src/main/java/com/jenkins/demo/controller/MessageController.java
@@ -0,0 +1,12 @@
+package com.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Hello, Jenkins!";
+    }
+}
